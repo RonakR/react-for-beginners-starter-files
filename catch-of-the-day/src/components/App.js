3eaf1a1e542b45e23658bebfb806c59343231ef2
Ronak Raithatha <ronaksraithatha@gmail.com>
2019-06-05 14:17:32 -0400

    added firebase persistance

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,7 @@ import Inventory from './Inventory'
 import Order from './Order'
 import Fish from './Fish'
 import sampleFishes from '../sample-fishes'
+import base from '../base'
 
 class App extends React.Component {
   // Ways to add state
@@ -17,6 +18,18 @@ class App extends React.Component {
   state = {
     fishes: {},
     order: {}
+  }
+
+  componentDidMount() {
+    const { params } = this.props.match
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: 'fishes'
+    })
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref)
   }
 
   addFish = fish => {
